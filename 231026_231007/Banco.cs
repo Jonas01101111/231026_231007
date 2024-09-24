ae4f29d32cbc75db9f625a0fa7efa1dacc2aba02
Jonas01101111 <jonas1kiubi@gmail.com>
2024-09-24 11:26:03 -0300

    Adicionar arquivos de projeto.

---
+++ 231026_231007/Banco.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace _231026_231007
+{
+    public class Banco
+    {
+        public static MySqlConnection Conexao;
+
+        public static MySqlCommand Comando;
+
+        public static MySqlDataAdapter Adaptador;
+
+        public static DataTable dataTable;
+
+    }
+
+
+
+
+
+
+
+
+
+
+}   
